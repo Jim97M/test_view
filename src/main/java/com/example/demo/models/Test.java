b681a19f7579614a31c2998c5bd7d2a6c2372b9f
Jim97M <wabuyajames@gmail.com>
2022-05-17 09:59:05 +0300

    Test Model

--- src/main/java/com/example/demo/models/Test.java
+++ src/main/java/com/example/demo/models/Test.java
@@ -1,6 +1,10 @@
 package com.example.demo.models;
 
+import sun.security.krb5.internal.ccache.Tag;
+
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "test")
@@ -12,6 +16,26 @@ public class Test {
     private String title;
     @Column(name = "description")
     private String description;
+    @Column(name="published")
+    private Boolean published;
+    @ManyToMany(fetch = FetchType.LAZY,
+     cascade = {
+           CascadeType.PERSIST,
+           CascadeType.MERGE
+     })
+    @JoinTable(name = "test_tags", joinColumns = {@JoinColumn(name="test_id")},
+     inverseJoinColumns = {@JoinColumn(name="tag_id")})
+    private Set<Tag> tags = new HashSet<>();
 
-    @ManyToMany
+    
+
+    public Test(){
+
+    }
+
+    public Test(String title, String description, Boolean published){
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
 }
