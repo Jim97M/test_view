63549db3169f326fd51ef3fd383f6dda73b558d7
Jim97M <wabuyajames@gmail.com>
2022-05-18 21:04:23 +0300

    Controller Addition

---
+++ src/main/java/com/example/demo/controllers/TestController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Test;
+import com.example.demo.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin("http://localhost:8080")
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @Autowired
+    TestRepository testRepository;
+    @GetMapping("/test")
+    public ResponseEntity<List<Test>> getAllTests(@RequestParam(required = false) String title) {
+        try {
+            List<Test> tests = new ArrayList<Test>();
+            if (title == null)
+                testRepository.findAll().forEach(tests::add);
+            if (tests.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tests, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/test/{id}")
+    public ResponseEntity<List<Test>> getTestById(@PathVariable("id") long id){
+        Optional<Test> testData = testRepository.findById(id);
+        if(testData.isPresent())
+            return new ResponseEntity<>(testData.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
