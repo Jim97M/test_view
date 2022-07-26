194d36f78705c28607e61b16458d4101222fa4e8
Jim97M <wabuyajames@gmail.com>
2022-07-26 15:39:51 +0300

    Webpack Config

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const miniCssExtraPlugin = require("mini-css-extract-plugin");
+ const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+ 
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index.bundle.js",
+      },
+      devServer: {
+        port: 3010,
+        liveReload: true,
+        historyApiFallback: true,
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+            },
+          },
+          {
+            test: /\.css$/,
+            use: [
+                miniCssExtraPlugin.loader,
+                "css-loader",
+            ],
+        },
+          {
+            test: /\.svg$/,
+            use: [
+              {
+                loader: 'svg-url-loader',
+                options: {
+                  limit: 10000,
+                },
+              },
+            ],
+          },
+        ],
+      },
+      plugins: [
+        new miniCssExtraPlugin(),
+         new HtmlWebpackPlugin({template: './src/index.html'})
+      ],
+      
+}
